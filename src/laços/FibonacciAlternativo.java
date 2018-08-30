1b02cc11a7022b0cbd489a7fe8d758a63d131b50
GabsFaria <gabriel-farinha2011@hotmail.com>
2018-08-30 09:41:32 -0300

    Novos exercícios sobre laços

---
+++ src/laços/FibonacciAlternativo.java
@@ -0,0 +1,27 @@
+package laços;
+
+//Fibonacci	usando	apenas	duas variáveis.
+
+public class FibonacciAlternativo {
+
+	public static void main(String[] args) {
+		int j = 1;
+		for (int i = 0; i <= 100;) {
+			if (i == 0) {
+				System.out.println(i);
+				i++;
+			} else if (i == 1) {
+				System.out.println(i);
+				System.out.println(i);
+				i++;
+			} else {
+				System.out.println(i);
+				i = i + j;
+				j = i - j;
+			}
+
+		}
+
+	}
+
+}
---
+++ src/laços/LaçosEncadeados.java
@@ -0,0 +1,24 @@
+package laços;
+
+import javax.swing.JOptionPane;
+
+public class LaçosEncadeados {
+
+	public static void main(String[] args) {
+		int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite o número:"));
+
+		if (numero <= 0) {
+			System.out.println("Número inválido!");
+		} else {
+			for (int i = 1; i <= numero; i++) {
+				for (int j = 1; j <= i; j++) {
+					int multiplicacao = i * j;
+					System.out.print(multiplicacao + " ");
+					multiplicacao = 0;
+				}
+				System.out.println();
+			}
+		}
+	}
+
+}
